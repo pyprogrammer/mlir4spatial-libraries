28268be1fb09530be91a8810e094f86a5aa40d07
Nathan Zhang <nzhang32@gmail.com>
2020-07-29 13:04:49 -0700

    Added parallelization for test.

--- test/mlir_libraries/tests/CoprocessorTests.scala
+++ test/mlir_libraries/tests/CoprocessorTests.scala
@@ -3,14 +3,14 @@ package mlir_libraries.tests
 import mlir_libraries.{CoprocessorScope, utils}
 import spatial.dsl._
 
-class TestProcessor(scope: mlir_libraries.CoprocessorScope) extends mlir_libraries.Coprocessor[I32, I32](2, 1, 1) {
+class TestProcessor(scope: mlir_libraries.CoprocessorScope, prealloc: scala.Int) extends mlir_libraries.Coprocessor[I32, I32](2, 1, prealloc) {
   override def coprocessorScope: CoprocessorScope = scope
   override def execute(inputs: Seq[I32]): Seq[I32] = {
     Seq((inputs reduceTree { _ + _ }) + I32(1))
   }
 }
 
-@spatial class CoprocessorTests(par_factor: scala.Int) extends SpatialTest {
+@spatial class CoprocessorTests(threads: scala.Int, workers: scala.Int) extends SpatialTest {
   val test_size = 16
 
   override def main(args: Array[String]): Void = {
@@ -22,12 +22,20 @@ class TestProcessor(scope: mlir_libraries.CoprocessorScope) extends mlir_librari
       Pipe {
         mlir_libraries.CoprocessorScope {
           scope =>
-            new TestProcessor(scope)
+            Range(0, workers) map {_ => new TestProcessor(scope, threads / workers)}
         } {
           proc =>
-            Foreach(0 until test_size, 0 until test_size par I32(par_factor)) {
-              case (i, j) =>
-                sram(i, j) = proc(Seq(i, j)).head
+            // split space
+            Range(0, threads) foreach {
+              block_id =>
+                val block_size = test_size / threads
+                val start = block_size * block_id
+                val end = start + block_size
+                val proc_id = block_id % workers
+                Foreach(0 until test_size, start until end) {
+                  case (i, j) =>
+                    sram(i, j) = proc(proc_id)(Seq(i, j)).head
+                }
             }
         }
         output store sram
@@ -49,4 +57,5 @@ class TestProcessor(scope: mlir_libraries.CoprocessorScope) extends mlir_librari
   }
 }
 
-class CoprocessorTest1 extends CoprocessorTests(1)
+class CoprocessorTest1 extends CoprocessorTests(1, 1)
+class CoprocessorTest4 extends CoprocessorTests(4, 2)
